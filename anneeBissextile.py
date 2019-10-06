85995307f978acda116fe3b713b0eac556e44347
GeorgesBOUM <gregoire.maison@icloud.com>
2019-10-06 16:31:24 +0200

    define a leap year

--- anneeBissextile.py
+++ anneeBissextile.py
@@ -1,4 +1,12 @@
-print("test the file running")
-salutation = "hello"
-salutation += " world !"
-print (salutation)
+annee = int(input("choisissez une année : "))
+
+if (annee % 4 == 0):
+	if (annee % 100 == 0):
+		if (annee % 400 == 0):
+			print("l'année {} est bissextile".format(annee))
+		else:
+			print("l'année {} n'est pas bissextile".format(annee))
+	else:
+		print("l'année {} est bissextile".format(annee))
+else:
+	print("l'année n'est {} pas bissextile".format(annee))
